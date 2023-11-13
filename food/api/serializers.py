3d9302e8a623c6857d1284e77bca57e6faeb13bd
Rustam Nazmutdinov <123296828+Rustamk0@users.noreply.github.com>
2023-11-13 23:55:21 +0300

    Update serializers.py

--- food/api/serializers.py
+++ food/api/serializers.py
@@ -32,18 +32,18 @@ class UserSerializer(UserSerializer):
       read_only_fields = ['is_subscribed']
       extra_kwargs = {'password': {'write_only': True}}
 
-  def create(self, validated_data):
-    user = User.objects.create_user(**validated_data)
-    user.set_password(validated_data['password'])
-    user.save()
-    return user
+    def create(self, validated_data):
+      user = User.objects.create_user(**validated_data)
+      user.set_password(validated_data['password'])
+      user.save()
+      return user
 
-  def get_is_subscribed(self, following):
-    request = self.context['request']
-    user = request.user
-    if user.is_anonymous:
-      return False
-    return Follow.objects.filter(user=user, following=following).exists()
+    def get_is_subscribed(self, following):
+      request = self.context['request']
+      user = request.user
+      if user.is_anonymous:
+        return False
+      return Follow.objects.filter(user=user, following=following).exists()
 
 
 class TagSerializer(serializers.ModelSerializer):
