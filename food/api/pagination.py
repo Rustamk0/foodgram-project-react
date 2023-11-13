183e5989f34f5546cf739ac3335872a196ccce92
Rustam Nazmutdinov <123296828+Rustamk0@users.noreply.github.com>
2023-11-13 23:06:49 +0300

    Update pagination.py

--- food/api/pagination.py
+++ food/api/pagination.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from food.settings import PAG_LIMIT, PAG_MAX_LIMIT
 
+
 class PagePagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_size = PAG_LIMIT
