4061093340f37a55c76bcf172ff60a393e6d2c73
Rustam Nazmutdinov <123296828+Rustamk0@users.noreply.github.com>
2023-11-14 21:59:54 +0300

    Update settings.py

--- food/food/settings.py
+++ food/food/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': [
-      'django_filters.rest_framework.DjangoFilterBackend'
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
 
