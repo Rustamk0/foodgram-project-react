760aaa54a5d4396030008622016663c6c4b2968c
Rustam Nazmutdinov <123296828+Rustamk0@users.noreply.github.com>
2023-11-14 18:48:50 +0300

    Update serializers.py

--- food/api/serializers.py
+++ food/api/serializers.py
@@ -39,7 +39,7 @@ class UserSerializer(UserSerializer):
         return user
 
 
-class TagSerializer(ModelSerializer):
+class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
