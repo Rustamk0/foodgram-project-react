15d3c2910a4c90a32dcd8b80ac0a09219e09ed46
Назмутдинлв Рустам <russmot@yandex.ru>
2023-11-17 23:58:54 +0300

    TAG

--- food/recipes/management/commands/load_tags.py
+++ food/recipes/management/commands/load_tags.py
@@ -1,5 +1,3 @@
-import csv
-
 from django.core.management.base import BaseCommand
 
 from recipes.models import Tag
@@ -8,9 +6,9 @@ from recipes.models import Tag
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-            data = [
-                {'name': 'Завтрак', 'color': '#ADFF2F', 'slug': 'breakfast'},
-                {'name': 'Обед', 'color': '#FF0000', 'slug': 'dinner'},
-                {'name': 'Ужин', 'color': '#00BFFF', 'slug': 'supper'}]
-            Tag.objects.bulk_create(Tag(**tag) for tag in data)
-            self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
+        data = [
+            {'name': 'Завтрак', 'color': '#ADFF2F', 'slug': 'breakfast'},
+            {'name': 'Обед', 'color': '#FF0000', 'slug': 'dinner'},
+            {'name': 'Ужин', 'color': '#00BFFF', 'slug': 'supper'}]
+        Tag.objects.bulk_create(Tag(**tag) for tag in data)
+        self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
