8cc4edce4224380e5febcee87bf5dd2d77cc152a
Назмутдинлв Рустам <russmot@yandex.ru>
2023-11-17 23:50:43 +0300

    PRINT

--- food/recipes/management/commands/load_tags.py
+++ food/recipes/management/commands/load_tags.py
@@ -7,9 +7,18 @@ from recipes.models import Tag
 
 class Command(BaseCommand):
 
+    # def handle(self, *args, **kwargs):
+        # with open("data/tags.csv", encoding="utf8") as f:
+            # reader_object = csv.reader(f, delimiter=",")
+            # for row in reader_object:
+                # obj = Tag(name=row[0], color=row[1], slug=row[2])
+                # obj.save()
+
+
     def handle(self, *args, **kwargs):
-        with open("data/tags.csv", encoding="utf8") as f:
-            reader_object = csv.reader(f, delimiter=",")
-            for row in reader_object:
-                obj = Tag(name=row[0], color=row[1], slug=row[2])
-                obj.save()
+            data = [
+                {'name': 'Завтрак', 'color': '#ADFF2F', 'slug': 'breakfast'},
+                {'name': 'Обед', 'color': '#FF0000', 'slug': 'dinner'},
+                {'name': 'Ужин', 'color': '#00BFFF', 'slug': 'supper'}]
+            Tag.objects.bulk_create(Tag(**tag) for tag in data)
+            self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
