33a13fe32419367f4f241dd9299242b02afcba37
Назмутдинлв Рустам <russmot@yandex.ru>
2023-11-17 23:55:18 +0300

    FLAKE

--- food/recipes/management/commands/load_tags.py
+++ food/recipes/management/commands/load_tags.py
@@ -7,14 +7,6 @@ from recipes.models import Tag
 
 class Command(BaseCommand):
 
-    # def handle(self, *args, **kwargs):
-        # with open("data/tags.csv", encoding="utf8") as f:
-            # reader_object = csv.reader(f, delimiter=",")
-            # for row in reader_object:
-                # obj = Tag(name=row[0], color=row[1], slug=row[2])
-                # obj.save()
-
-
     def handle(self, *args, **kwargs):
             data = [
                 {'name': 'Завтрак', 'color': '#ADFF2F', 'slug': 'breakfast'},
